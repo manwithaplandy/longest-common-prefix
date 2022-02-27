a164953853808e68947ab5d1c5e17a9fa9f11a8c
manwithaplandy <abysmalduck@gmail.com>
2022-02-27 10:41:39 -0800

    "Do not do what has already been done for you" -- solution found using os module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
@@ -9,21 +11,6 @@ class Solution(object):
          the previous word, save that
 
         """
-
-        longest_prefix = ''
-        running_prefix = ''
-        last_word = str[0]  # First word is the first last_word
-
-        for word in str[1:]:  # iterate through all of the strings in the list after the first one
-            letter_count = 0
-            for letter in word:  # iterate through the letters in the word
-                if letter != longest_prefix[letter_count] and letter != running_prefix[letter_count]:
-                    # If no matches, go to the next word
-                    break
-                elif letter == longest_prefix[letter_count]:
-                    longest_prefix += letter
-                    continue
-                elif letter == last_word[letter_count] and letter != longest_prefix[letter_count]:
-
-
-
+        answer = ""
+        answer = os.path.commonprefix(strs)
+        return(answer)
